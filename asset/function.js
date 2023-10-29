397272e5453c17ed29eef310dc028fe40cc1b5b8
thuantruong00 <taithuan2000@gmail.com>
2023-10-29 23:57:29 +0700

    a

---
+++ asset/function.js
@@ -0,0 +1,6 @@
+// let href = window.location.href.split("/"); //====
+// let host = window.location.host.split(":"); //====
+// let path = window.location.protocol + "//" + host[0] + ":" + host[1]; //dev
+// // let path = window.location.protocol + "//" + host[0] // deploy
+// // let path = "https://hecwin.com" // deploy
+// console.log(path)
---
+++ readme.txt
@@ -0,0 +1,15 @@
+# header, footer se bi thay doi noi dung tai folder ./template/header/script
+<!-- replace{ -->  <phan html tag bi thay doi noi dung>   <!-- }replace -->
+
+#he thong
+./asset/
+./bootstrap/
+./component/
+./edit-style/
+./image/
+./template/
+./readme.txt
+
+#router
+./index.html
+./giai-phap/
