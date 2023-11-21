a098940d9635ab7836ab7837c43c252997043cd2
thuantruong00 <taithuan2000@gmail.com>
2023-11-21 15:33:20 +0700

    s2

--- component/slider/script.js
+++ component/slider/script.js
@@ -20,3 +20,23 @@ $(document).ready(function () {
 
     });
 });
+$(document).ready(function () {
+    $("#slider-intro").owlCarousel({
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        },
+        autoplay: true,
+        autoplayTimeout: 4000,
+        dots: false
+
+    });
+});
