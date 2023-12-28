aa08c8fb6d218af35e39c39fb1e50fccdfe48e9b
thuantruong00 <taithuan2000@gmail.com>
2023-12-28 12:27:25 +0700

    rang-su-page

--- asset/function.js
+++ asset/function.js
@@ -1,12 +1,25 @@
 let href = window.location.href.split("/"); //====
 let host = window.location.host.split(":"); //====
-// let path = window.location.protocol + "//" + host[0] + ":" + host[1]; //dev
+let path = window.location.protocol + "//" + host[0] + ":" + host[1]; //dev
 // let path = window.location.protocol + "//" + host[0] // deploy
 
-let path = 'https://thuantruong00.github.io/nhakhoahoakimcuong-ui' //git
+// let path = 'https://thuantruong00.github.io/nhakhoahoakimcuong-ui' //git
 // console.log(path)
 
+let click_out = true
 
+
+$(window).on("scroll", function(){
+    let page_y_offset = window.scrollY
+    
+    // console.log(page_y_offset)
+    if(page_y_offset>400){
+        $(".header").addClass("sticky-menu")
+    }
+    if(page_y_offset<200){
+        $(".header").removeClass("sticky-menu")
+    }
+})
 
 
 let countdown = 1800;
@@ -21,3 +34,22 @@ let interval_func = setInterval(function(){
         clearInterval(interval_func);
     }
 }, 1000);
+
+$('.navbar').click(function(){
+    openMenu2();
+})
+
+function openMenu2(){
+    click_out=false
+    $('#menu-2').toggleClass('move-in')
+    setTimeout(() => {
+        
+        click_out=true
+    }, 1000);
+}
+
+$(window).click(function(){
+    if(click_out){
+        $('#menu-2').removeClass('move-in')
+    }
+})
--- component/header/script.js
+++ component/header/script.js
@@ -4,6 +4,10 @@ let menu_data = [
         link:"#dich-vu"
     },
     {
+        title:"Thẩm mỹ <br>răng sứ",
+        link:"#"
+    },
+    {
         title:"Bảng giá",
         link:"/bang-gia"
     },
@@ -16,19 +20,99 @@ let menu_data = [
         link:"#nhan-tu-van"
     }
 ]
+let child_rangsu = [
+    {
+        category:"Răng sứ toàn phần cao cấp",
+        data:[
+            {
+                title:"Răng sứ Orodent",
+                link:"rang-su-orodent"
+            },
+            {
+                title:"Răng sứ Lava",
+                link:"rang-su-lava"
+            },
+            {
+                title:"Răng sứ Cercon",
+                link:"rang-su-cercon"
+            }
+        ]
+    },
+    {
+        category:"Răng sứ toàn phần",
+        data:[
+            {
+                title:"Răng sứ HT Smile",
+                link:"rang-su-ht-smile"
+            },
+            {
+                title:"Răng sứ DDBIO",
+                link:"rang-su-ddbio"
+            },
+            {
+                title:"Răng sứ Zirconia",
+                link:"rang-su-zirconia"
+            }
+        ]
+    },
+    {
+        category:"Mặt dán sứ Veneer Laminate",
+        data:[
+            {
+                title:"Dán sứ Veneer 3D Max",
+                link:"dan-su-veneer-3d-max"
+            },
+            {
+                title:"Dán sứ Veneer Ultrathin",
+                link:"dan-su-veneer-ultrathin"
+            },
+            {
+                title:"Dán sứ Veneer EMax",
+                link:"dan-su-veneer-emax"
+            }
+        ]
+    },
+]
 let str = '';
-for (const key of menu_data) {
-    console.log(key)
-    str += `<li><a href="${path+key.link}">${key.title}</a></li>`;
-
+let sub_menu = '';
+let str_2 = '';
+let sub_menu_2 = '';
+for (let item of child_rangsu) {
+    // console.log(item.category)
+    sub_menu +=`<div class="wrap-col"><div class="category-name">${item.category}</div><div class="wrap-list"><ul>`;
+    for (let item_1 of item.data) {
+        // console.log(item_1)
+        sub_menu +=`<li><a href="${item_1.link}" class="">${item_1.title}</a></li>`;
+    }
+    sub_menu +=`</ul></div></div>`;
+}
+sub_menu = `<div class="wrap-sub-menu"><div class="sub-menu">${sub_menu}</div></div>`
+for (let key in menu_data) {
+    // console.log(key)
+    if(key==1){
+        str += `<li><a href="javascript:openMenu2()">${menu_data[key].title}</a>${sub_menu}</li>`;
+    }
+    else{
+        str += `<li><a href="${path+menu_data[key].link}">${menu_data[key].title}</a></li>`;
+    }
+}
+for (let item of child_rangsu) {
+    // console.log(item.category)
+    sub_menu_2 +=`<h5>${item.category}</h5><ul>`;
+    for (let item_1 of item.data) {
+        // console.log(item_1)
+        sub_menu_2 +=`<li><a href="${item_1.link}" class="">${item_1.title}</a></li>`;
+    }
+    sub_menu_2 +=`</ul>`;
 }
 
 str = `<ul>${str}<li class="d-lg-block d-none"><a href="tel:0966.949.839" class="phone"><i class="fa fa-phone"></i></a></li></ul>`;
-$("#main-menu").html(str)
-
+$("#main-menu").html($(str))
+str_2 = `<div class="sub-menu-2"><h4 class="text-center">Thẩm mỹ răng sứ</h4>${sub_menu_2}</div>`
+$("#menu-2").html($(str_2))
 // image path
 $("img").each(function(e){
     let src = $(this).attr('src');
-    console.log(src)
+    // console.log(src)
     $(this).attr('src', path+src)
 })
